c13bc801ee030245fea6b7773fbd70ed9911988e
DmitriyLitvin <dimalitvin192.0@gmail.com>
2020-07-28 01:22:41 +0300

    added quit in the end

--- selenium_selectors.py
+++ selenium_selectors.py
@@ -9,6 +9,7 @@ https://the-internet.herokuapp.com/challenging_dom#delete
 """
 from time import sleep
 
+from pip._vendor.urllib3.util import wait
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -62,3 +63,5 @@ for row in table:
         print(cell.text + " "),
         i += 1;
     print ("")
+
+driver.quit()
